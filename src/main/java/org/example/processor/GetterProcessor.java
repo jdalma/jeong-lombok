f5a2f3d02b04a29a3ff2d34a9b44a942fa8859b7
jeongcode <58499373+jeongcode@users.noreply.github.com>
2021-03-28 21:05:09 +0900

    PoetGetter 완료

--- src/main/java/org/example/processor/GetterProcessor.java
+++ src/main/java/org/example/processor/GetterProcessor.java
@@ -1,9 +1,10 @@
-package org.example;
+package org.example.processor;
 
 
 import com.google.auto.service.AutoService;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.util.TreePath;
 import com.sun.source.util.TreePathScanner;
 import com.sun.source.util.Trees;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
@@ -13,17 +14,19 @@ import com.sun.tools.javac.tree.TreeTranslator;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.List;
 import com.sun.tools.javac.util.Names;
+import org.example.annotation.JeongGetter;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
 import java.util.Set;
 
 @AutoService(Processor.class)
-@SupportedAnnotationTypes("org.example.JeongGetter")
+@SupportedAnnotationTypes("org.example.annotation.JeongGetter")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class GetterProcessor extends AbstractProcessor {
 
@@ -48,25 +51,25 @@ public class GetterProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        System.out.println("call process =" + annotations);
         TreePathScanner<Object, CompilationUnitTree> scanner = new TreePathScanner<Object, CompilationUnitTree>(){
             @Override
             public Trees visitClass(ClassTree classTree, CompilationUnitTree unitTree){
-                System.out.println(classTree);
-                System.out.println(unitTree);
                 JCTree.JCCompilationUnit compilationUnit = (JCTree.JCCompilationUnit) unitTree;
-                System.out.println(compilationUnit);
                 if (compilationUnit.sourcefile.getKind() == JavaFileObject.Kind.SOURCE){
                     compilationUnit.accept(new TreeTranslator() {
                         @Override
                         public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
                             super.visitClassDef(jcClassDecl);
-                            List<JCTree> members = jcClassDecl.getMembers();
-                            for(JCTree member : members){
-                                if (member instanceof JCTree.JCVariableDecl){
-//                                    List<JCTree.JCMethodDecl> getters = createGetter((JCTree.JCVariableDecl) member);
-//                                    for(JCTree.JCMethodDecl getter : getters){
-//                                        jcClassDecl.defs = jcClassDecl.defs.prepend(getter);
-//                                    }
+                            List<JCTree> fields = jcClassDecl.getMembers();
+                            System.out.println("class name = " + jcClassDecl.getSimpleName());
+                            for(JCTree field : fields){
+                                System.out.println("class field = " + field);
+                                if (field instanceof JCTree.JCVariableDecl){
+                                    List<JCTree.JCMethodDecl> getters = createGetter((JCTree.JCVariableDecl) field);
+                                    for(JCTree.JCMethodDecl getter : getters){
+                                        jcClassDecl.defs = jcClassDecl.defs.prepend(getter);
+                                    }
                                 }
                             }
                         }
@@ -75,7 +78,34 @@ public class GetterProcessor extends AbstractProcessor {
                 return trees;
             }
         };
+
+        for (final Element element : roundEnv.getElementsAnnotatedWith(JeongGetter.class)) {
+            System.out.println("call process - getPath() element = " + element);
+            if(element.getKind() != ElementKind.CLASS){
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "@JeongGetter annotation cant be used on" + element.getSimpleName());
+            }else{
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "@JeongGetter annotation Processing " + element.getSimpleName());
+                final TreePath path = trees.getPath(element);
+                scanner.scan(path, path.getCompilationUnit());
+            }
+        }
         return true;
     }
 
+    public List<JCTree.JCMethodDecl> createGetter(JCTree.JCVariableDecl var){
+        String str = var.name.toString();
+        String upperVar = str.substring(0,1).toUpperCase()+str.substring(1,var.name.length());
+        System.out.println(str + " Create Getter");
+        return List.of(
+                // treeMaker.Modifiers -> syntax tree node 에 접근하여 수정 및 삽입하는 역할
+                treeMaker.MethodDef(
+                        treeMaker.Modifiers(1), // public
+                        names.fromString("get".concat(upperVar)), // 메서드 명
+                        (JCTree.JCExpression) var.getType(), // return type
+                        List.nil(),
+                        List.nil(),
+                        List.nil(),
+                        treeMaker.Block(1, List.of(treeMaker.Return((treeMaker.Ident(var.getName()))))),
+                        null));
+    }
 }
